87aeab9c2e9626df893a15488a81a2c6d24506ba
kleber89 <benkosisackson@gmail.com>
2024-05-02 14:39:49 -0500

    function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,61 +1,38 @@
 #include "sort.h"
 
 /**
- * swap - Swaps two elements in an array.
- * @array: Array of integers.
- * @i: Index of first element.
- * @j: Index of second element.
- */
-void swap(int *array, size_t i, size_t j)
-{
-	/* Store value at index i */
-	int temp = array[i];
-	/* Assign value at index j to index i */
-	array[i] = array[j];
-	/* Assign stored value to index j */
-	array[j] = temp;
-}
-
-/**
- * bubble_sort - Sorts an array of integers using Bubble sort algorithm.
- * @array: Array of integers to be sorted.
- * @size: Size of array.
- *
- * Description: If array is null or size is less than 2, function returns.
- * Iterates over array, swapping adjacent elements if they are in wrong order.
- * Iteration continues until no more swaps are made.
+ * bubble_sort - Sorts an array of integers
+ * in ascending order using Bubble sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	/* Loop counter */
-	size_t i;
-	/* Flag to check if swap occurred */
+	size_t i, j;
+	int temp;
 	int swapped;
 
-	/* Return if array is null or size is less than 2 */
-	if (!array || size < 2)
-		return;
-
-	/* Repeat until no more swaps are made */
-	do
+	for (i = 0; i < size - 1; i++)
 	{
-		/* Reset swapped flag */
 		swapped = 0;
-
-		/* Loop through array */
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			/* If current element is less than or equal to next one, continue to next iteration */
-			if (array[i] <= array[i + 1])
-				continue;
+			if (array[j] > array[j + 1])
+			{
+				/* Swap elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-			/* Swap elements */
-			swap(array, i, i + 1);
-			/* Set swapped flag to 1 */
-			swapped = 1;
-			/* Print array after swap */
-			print_array(array, size);
+				/* Print array after each swap */
+				print_array(array, size);
+
+				swapped = 1;
+			}
 		}
-		/* Continue if a swap was made */
-	} while (swapped);
+
+		/* If no two elements were swapped, the array is already sorted */
+		if (swapped == 0)
+			break;
+	}
 }
